0ed712c832570de0af78638e53e39b71e6c26eeb
Matteo De Sabato <desabatomatteo@yahoo.it>
2023-03-17 14:34:23 +0100

    Can now differentiate between returning a single character or the complete list of characters

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -12,13 +12,24 @@ namespace web_api.Controllers
     public class CharacterController : ControllerBase
     {
         // Static mock character
-        private static Character fighter = new Character();
+        private static List<Character> characters = new List<Character>() {
+            new Character(),
+            new Character{ Id = 1, Name = "Douchebag"}
+        };
 
-        [HttpGet]
-        public ActionResult<Character> Get() {
+        [HttpGet("GetAll")]
+        public ActionResult<List<Character>> GetAll() {
 
             // Ok sends back a status code 200
-            return Ok(fighter);
+            return Ok(characters);
+        }
+
+        // Only return a single character. For now return the first
+        [HttpGet("GetSingle/{id}")]
+        public ActionResult<Character> GetSingle(int id) {
+
+            
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
         }
     }
 }
