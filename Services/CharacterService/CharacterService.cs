fa6e589d627bb49320dbf6476f64dfa6c3834138
Matteo De Sabato <desabatomatteo@yahoo.it>
2023-05-15 19:18:53 +0200

    All character CRUD methods are secured

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
 
@@ -11,27 +12,37 @@ namespace web_api.Services.CharacterService
 
         private readonly DataContext _context;
         private readonly IMapper _mapper;
-        public CharacterService(IMapper mapper, DataContext context)
+
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public CharacterService(IMapper mapper, DataContext context, IHttpContextAccessor httpContextAccessor)
         {
             _context = context;
             _mapper = mapper;
+            _httpContextAccessor = httpContextAccessor;
         }
 
+        private int GetUserId() => int.Parse(_httpContextAccessor.HttpContext!.User
+            .FindFirstValue(ClaimTypes.NameIdentifier)!);
         public async Task<ServiceResponse<List<GetCharacterDTO>>> AddCharacter(AddCharacterDTO newCharacter)
         {
             var serviceResponse = new ServiceResponse<List<GetCharacterDTO>>();
             var character = _mapper.Map<Character>(newCharacter);
+            character.User = await _context.Users.FirstOrDefaultAsync(u => u.Id == GetUserId());
+
             _context.Characters.Add(character);
             await _context.SaveChangesAsync();
             serviceResponse.Data = 
-                await _context.Characters.Select(c => _mapper.Map<GetCharacterDTO>(c)).ToListAsync();
+                await _context.Characters
+                    .Where(u => u.User!.Id == GetUserId())
+                    .Select(c => _mapper.Map<GetCharacterDTO>(c))
+                    .ToListAsync();
             return serviceResponse;
         }
 
         public async Task<ServiceResponse<List<GetCharacterDTO>>> GetAllCharacters()
         {
             var serviceResponse = new ServiceResponse<List<GetCharacterDTO>>();
-            var dbCharacters = await _context.Characters.ToListAsync();
+            var dbCharacters = await _context.Characters.Where(c => c.User!.Id == GetUserId()).ToListAsync();
             serviceResponse.Data = dbCharacters.Select(c => _mapper.Map<GetCharacterDTO>(c)).ToList();
             return serviceResponse;
         }
@@ -39,7 +50,8 @@ namespace web_api.Services.CharacterService
         public async Task<ServiceResponse<GetCharacterDTO>> GetCharacterById(int id)
         {
             var serviceResponse = new ServiceResponse<GetCharacterDTO>();
-            var dbCharacter = await _context.Characters.FirstOrDefaultAsync(c => c.Id == id);
+            var dbCharacter = await _context.Characters
+                .FirstOrDefaultAsync(c => c.Id == id && c.User!.Id == GetUserId());
             serviceResponse.Data = _mapper.Map<GetCharacterDTO>(dbCharacter);
             return serviceResponse;
         }
@@ -49,9 +61,11 @@ namespace web_api.Services.CharacterService
             var serviceResponse = new ServiceResponse<GetCharacterDTO>();
             try {
                 var character = 
-                    await _context.Characters.FirstOrDefaultAsync(c => c.Id == updatedCharacter.Id);
+                    await _context.Characters
+                        .Include(c => c.User)
+                        .FirstOrDefaultAsync(c => c.Id == updatedCharacter.Id);
 
-                if(character is null) {
+                if(character is null || character.User!.Id != GetUserId()) {
                     throw new Exception($"Character with Id '{updatedCharacter.Id}' not found");
                 }
                 
@@ -74,7 +88,8 @@ namespace web_api.Services.CharacterService
             var serviceResponse = new ServiceResponse<List<GetCharacterDTO>>();
             try {
                 var character = 
-                    await _context.Characters.FirstAsync(c => c.Id == id);
+                    await _context.Characters
+                        .FirstAsync(c => c.Id == id && c.User!.Id == GetUserId());
 
                 if(character is null) {
                     throw new Exception($"Character with Id '{id}' not found");
@@ -85,7 +100,9 @@ namespace web_api.Services.CharacterService
 
 
                 serviceResponse.Data = 
-                    await _context.Characters.Select(c => _mapper.Map<GetCharacterDTO>(c)).ToListAsync();
+                    await _context.Characters
+                    .Where(u => u.User!.Id == GetUserId())
+                    .Select(c => _mapper.Map<GetCharacterDTO>(c)).ToListAsync();
             } catch (Exception e) {
                 serviceResponse.Success = false;
                 serviceResponse.Message = e.Message;
